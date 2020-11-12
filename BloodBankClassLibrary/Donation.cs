86f23771d97da49202c14ca664dcedeee46a70c6
Mathias Millberg <millberg_m@hotmail.com>
2020-11-12 12:09:07 +0100

    ..

--- BloodBankClassLibrary/Donation.cs
+++ BloodBankClassLibrary/Donation.cs
@@ -2,17 +2,17 @@ namespace Bloodbank
 {
     class Donation
     {
-        public Bloodgroup Bloodgroup { get; set; }
+        public BloodGroup Bloodgroup { get; set; }
         public int AmountOfBlood { get; set; }
         public long DonorID { get; set; }
         public long StaffID { get; set; }
 
-        public Donation (Bloodgroup bloodgroup, int amountOfBlood, long donorID, long staffID)
+        public Donation (BloodGroup bloodgroup, int amountOfBlood, long donorID, long staffID)
         {
             this.Bloodgroup = bloodgroup;
             this.AmountOfBlood = amountOfBlood;
             this.DonorID = donorID;
-            this.StaffID = staffID
+            this.StaffID = staffID;
         }
     }
 }
