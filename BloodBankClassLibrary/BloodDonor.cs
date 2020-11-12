ec3cf64d78d8ddcec6a1a6ef2b835b377f4e6bf7
Mathias Millberg <millberg_m@hotmail.com>
2020-11-12 10:30:03 +0100

    Co-authored-by: Irene Ku Iveling <82irekui@skola.boras.se>

--- BloodBankClassLibrary/BloodDonor.cs
+++ BloodBankClassLibrary/BloodDonor.cs
@@ -10,21 +10,8 @@ namespace Bloodbank
         public string Email { get; set; }
         public bool AvailableToDonate { get; set; } = true;
         public bool HealthOK { get; set; } = true;
-        public int BloodType { get; set; }
+        public Bloodtype bloodtype
         public DateTime LatestDonation { get; set; }
-
-        public BloodDonor (string firstname, string lastname, int idnumber, string email, Bloodtype bloodtype)
-        {
-            this.FirstName = firstname;
-            LastName = lastname;
-            IDNumber = idnumber;
-            Email = email;
-            BloodType = bloodtype;
-            
-
-        }
-
-
 
     }
 }
