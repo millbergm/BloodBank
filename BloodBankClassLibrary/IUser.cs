3f3d1c9a6abfcbe1366688f237b1dc0e53d9bdd3
Rikard Friberg <ri.frib@gmail.com>
2020-11-12 09:32:33 +0100

    aoeae

--- BloodBankClassLibrary/IUser.cs
+++ BloodBankClassLibrary/IUser.cs
@@ -2,8 +2,8 @@ namespace Bloodbank
 {
     interface IUser
     {
-        string Firstname { get; set; }
-        string Lastname { get; set; }
-        int IDnumber { get; set;}
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        int IDNumber { get; set;}
     }
 }
--- BloodBankClassLibrary/interfaces.cs
+++
@@ -1,9 +0,0 @@
-namespace Bloodbank
-{
-    interface IUser
-    {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public int IDnumber { get; set;}
-    }
-}
