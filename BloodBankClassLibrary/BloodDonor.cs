16a7222603fed3ef5257d77bd6c7c8ec74845c39
Mathias Millberg <millberg_m@hotmail.com>
2020-11-12 10:34:27 +0100

    skapat class Blooddonor

--- BloodBankClassLibrary/BloodDonor.cs
+++ BloodBankClassLibrary/BloodDonor.cs
@@ -10,8 +10,21 @@ namespace Bloodbank
         public string Email { get; set; }
         public bool AvailableToDonate { get; set; } = true;
         public bool HealthOK { get; set; } = true;
-        public Bloodtype bloodtype
+        public int BloodType { get; set; }
         public DateTime LatestDonation { get; set; }
+
+        public BloodDonor (string firstname, string lastname, int idnumber, string email, Bloodtype bloodtype)
+        {
+            this.FirstName = firstname;
+            LastName = lastname;
+            IDNumber = idnumber;
+            Email = email;
+            BloodType = bloodtype;
+            
+
+        }
+
+
 
     }
 }
